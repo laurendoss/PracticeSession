7bd2725cbe9d132df59f9db44b905d42b89234b0
Lauren Doss <doss.laurene@gmail.com>
2020-08-27 10:56:23 -0700

    updated counting valleys algorithm

--- practice.js
+++ practice.js
@@ -4,3 +4,30 @@ function palindrome(str) {
     });
   }
   
+  function countingValleys(n, s) {
+    let seaLevel = 0;
+    let currLevel = 0;
+    let valleys = 0;
+    
+    s = s.split('');
+
+    //counting valleys
+    for (let i = 0; i < s.length; i++) {
+        //update the current level
+        if (s[i] === 'U') {
+            currLevel += 1;
+            if (currLevel == 0) {
+                valleys += 1;
+            }
+        } else {
+            currLevel -= 1;
+        }
+        
+        
+    }
+    
+    return valleys;
+
+}
+
+
